88307b8e74fcebf3ac33ba2191b927620a9dd4de
Mister Souza <mistersouza@icloud.com>
2023-12-29 15:47:34 +0100

    Write sidebar component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+    return (
+        <div>
+            <a href='#!' className='sidebar'>My photos</a>
+            <a href='#!' className='Sidebar'>My illustrations</a>
+            <a href='#!' className='Sidebar'>My paintings</a>
+        </div>
+    )
+}
+
+export default Sidebar
