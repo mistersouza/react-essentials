db2ede1367d99c4a8608d6f6c7f9592654cec2de
Mister Souza <mistersouza@icloud.com>
2024-01-02 22:12:25 +0100

    Introduction to Component Lifecycles

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react'
 import PostItem from './PostItem'
+import Loader from './Loader'
 import { savedPosts as posts } from '../posts.json'
 import css from './css/Content.module.css'
 
 export class Content extends Component {
     constructor(props) {
       super(props)
-    
       this.state = {
-         first: 1
+        isLoaded: false
       }
     }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true
+            })
+        }, 2000)
+    }
+
   render() {
     return (
-        <div>
+        <div className={css.content}>
             <div className={css.titleBar}>
                 <h1>My Photos</h1>
             </div>
             <div className={css.searchResults}>
-                <PostItem posts={posts} />
+                {
+                    this.state.isLoaded 
+                    ? <PostItem posts={posts} />
+                    : <Loader />
+                }
             </div>
         </div>
     )}
